89e50eb1317ff9168247b7013ad117c4ea2199b0
Ali Ravari <alirezakarbakhshr@gmail.com>
2021-08-04 21:00:25 -0500

    almost done

--- generator.py
+++ generator.py
@@ -1,1 +1,62 @@
 from torch import nn
+from torch.nn.modules.activation import GELU
+import torch 
+
+class Generator(nn.Module):
+    def __init__(self, latent_dim: int = 100):
+        super(Generator, self).__init__()
+
+        self.layers = nn.Sequential(
+            Up(latent_dim,75),
+            Up(75, 50),
+            Up(50, 25),
+            Up(25,20),
+            Up(20, 15),
+            Up(15, 10),
+            Up(10, 5),
+            Up(5,3)
+        )
+
+    def forward (self, x):
+        return self.layers(x)
+
+
+
+
+class DoubleConv(nn.Module):
+    """(convolution => [BN] => ReLU) * 2"""
+
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+class Up(nn.Module):
+    """Upscaling then double conv"""
+
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super().__init__()
+
+        # if bilinear, use the normal convolutions to reduce the number of channels
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels , in_channels // 2, kernel_size=2, stride=2)
+            self.conv = DoubleConv(in_channels, out_channels)
+
+
+    def forward(self,x):
+        x = self.up(x)
+        return self.conv(x)
--- train.py
+++ train.py
@@ -1,53 +1,30 @@
-import math
-import pytorch_lightning as pl
 import torch
-from torch.optim import Adam, Optimizer
-from torch.optim.lr_scheduler import LambdaLR
+import pytorch_lightning as pl
+from argparse import ArgumentParser
+from torchvision.datasets import DatasetFolder
+from PIL import Image
+from torch.utils.data import DataLoader
+from torchvision.transforms import ToTensor
+from painting_gan import PaintingGan
 
-
-class PaintingGan(pl.LightningModule):
-    def __init__(self, num_classes: int, learning_rate: float = 1e-3) -> None:   
-        super().__init__()
-        self.save_hyperparameters() # instead of self.num_classes etc.
-
-    def configure_optimizers(self):
-        gen_adam = Adam(lr=self.hparams.learning_rate, betas= (0.5, 0.999))
-        dis_adam = Adam(lr=self.hparams.learning_rate, betas= (0.5, 0.999))
-        return [gen_adam, dis_adam] , [get_cosine_schedule_with_warmup(gen_adam, 1000, 100_000), get_cosine_schedule_with_warmup(dis_adam, 1000, 100_000)]
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("--data", default="resizedImages", type=str, help="image directory")# -- means optional
+    args = parser.parse_args()
     
-    def training_step(self, batch, batch_idx):
-        return 
+    dataset = DatasetFolder(args.data, Image.open, transform= ToTensor(), extensions=(".jpg",))
+    num_datapoints = len(dataset)
+    test_datapoints = int(0.1*num_datapoints) # 10 percent of the data for training
+    train_datapoints = num_datapoints - test_datapoints
+    train, test = torch.utils.data.random_split(dataset, (train_datapoints, test_datapoints))
+    train_dl = DataLoader(train, 32, shuffle=True, num_workers=11)
+    test_dl = DataLoader(test, 32, num_workers=11)
+    Model = PaintingGan(num_classes=len(dataset.classes))
+
+    trainer = pl.Trainer(gpus= 1, max_epochs=100)
+    trainer.fit(Model, train_dataloader= train_dl, val_dataloaders= test_dl)
 
 
-def get_cosine_schedule_with_warmup(
-    optimizer: Optimizer, num_warmup_steps: int, num_training_steps: int, num_cycles: float = 0.5, last_epoch: int = -1
-):
-    """
-    Create a schedule with a learning rate that decreases following the values of the cosine function between the
-    initial lr set in the optimizer to 0, after a warmup period during which it increases linearly between 0 and the
-    initial lr set in the optimizer.
 
-    Args:
-        optimizer (:class:`~torch.optim.Optimizer`):
-            The optimizer for which to schedule the learning rate.
-        num_warmup_steps (:obj:`int`):
-            The number of steps for the warmup phase.
-        num_training_steps (:obj:`int`):
-            The total number of training steps.
-        num_cycles (:obj:`float`, `optional`, defaults to 0.5):
-            The number of waves in the cosine schedule (the defaults is to just decrease from the max value to 0
-            following a half-cosine).
-        last_epoch (:obj:`int`, `optional`, defaults to -1):
-            The index of the last epoch when resuming training.
-
-    Return:
-        :obj:`torch.optim.lr_scheduler.LambdaLR` with the appropriate schedule.
-    """
-
-    def lr_lambda(current_step):
-        if current_step < num_warmup_steps:
-            return float(current_step) / float(max(1, num_warmup_steps))
-        progress = float(current_step - num_warmup_steps) / float(max(1, num_training_steps - num_warmup_steps))
-        return max(0.0, 0.5 * (1.0 + math.cos(math.pi * float(num_cycles) * 2.0 * progress)))
-
-    return LambdaLR(optimizer, lr_lambda, last_epoch)
+if __name__ == "__main__":
+    main()
